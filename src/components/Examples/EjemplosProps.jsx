01b95a44dbfe622b4d31e4bb1ce44d9fb002c224
PuntoExe <exequielguardia@gmail.com>
2024-04-18 15:05:50 -0300

    ejemplos de calse

---
+++ src/components/Examples/EjemplosProps.jsx
@@ -0,0 +1,11 @@
+const EjemplosProps = ({msj, image}) => {
+
+  return (
+    <div>
+      <h2>{msj}</h2>
+      <img src={image}/>
+    </div>
+  )
+}
+
+export default EjemplosProps
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+
+    return(
+        <nav>
+            <h2 className="h2nav">Componente NavBar</h2>
+            <ul>.teclado</ul>
+            <ul>.mouse</ul>
+        </nav>
+    )
+};
+export default NavBar
